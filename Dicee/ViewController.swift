2d1cdc7007d5db5abf011823d8a0c7be0eb7572a
Vincent <v@crowdswap.com>
2019-05-19 13:24:54 -0500

    Cleaned some empty spaces

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
+    @IBOutlet weak var labelDice1: UILabel!
+    
+    @IBOutlet weak var labelDice2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDiceNumbers()
     }
 
-
+    @IBAction func buttonRoll(_ sender: UIButton) {
+        updateDiceNumbers()
+    }
+    
+    func updateDiceNumbers() {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        labelDice1.text = "\(randomDiceIndex1)"
+        labelDice2.text = "\(randomDiceIndex2)"
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            updateDiceNumbers()
+        }
+    }
+    
 }
-
